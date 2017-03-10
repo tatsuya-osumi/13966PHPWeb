e1e2c9f265e6d31f7f5d417a5896f20673485fcf
tatsuya-osumi <t.osumi@sankei.co.jp>
2017-03-11 01:50:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World! IN PHP"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
+ <head>
+  <title>13966 OsumiWebsite</title>
+ </head>
+ <body>
+  <h1>テストページです。</h1>
+  <a href="form.htm">フォーム</a><br/>
+  <a href="conf.htm">投稿後の画面</a><br/>
+  <a href="comp.htm">確認画面</a><br/>
+ </body>
 </html>
