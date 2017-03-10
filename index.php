d67d34ae19c3d2876fbc56d2c73977ef3790ef46
tatsuya-osumi <t.osumi@sankei.co.jp>
2017-03-11 01:47:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,24 +6,6 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
-	<?php
-    // mysqlへのアクセス
-    $mysqli = new mysqli("$hostname:$port","$username","$password", "$name");
-    // sql文の発行
-    $result = $mysqli->query("select * from test");
-    // 取得した行分繰り返し
-    while ($row = $result->fetch_row()) {
-        //　テーブルの列分繰り返し 
-　　　　　　　　　　　　　　　　for($i = 0; $i < mysqli_num_fields ( $result ); $i ++) {
-            // 要素の表示
-            echo "$row[$i]";
-        }
-    }
-    // 結果を開放
-    result->close();
-    // 接続を閉じる
-    $mysqli->close();
-?>
 	<table>
 		<tr>
 			<td style='width: 30%;'>
