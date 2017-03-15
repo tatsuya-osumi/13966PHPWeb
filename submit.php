e0ca2f765bd52c8e3c391131de783edfd8900edc
tatsuya-osumi <t.osumi@sankei.co.jp>
2017-03-15 12:27:44 +0900

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>アドレス登録</title>
+</head>
+<body>
+<?php
+
+$con = mysql_connect('us-cdbr-iron-east-03.cleardb.net:3306', 'b1b8e502d991e6', 'e52e3992');
+if (!$con) {
+  exit('データベースに接続できませんでした。');
+}
+
+$result = mysql_select_db('phpdb', $con);
+if (!$result) {
+  exit('データベースを選択できませんでした。');
+}
+
+$result = mysql_query('SET NAMES utf8', $con);
+if (!$result) {
+  exit('文字コードを指定できませんでした。');
+}
+
+$no   = $_REQUEST['no'];
+$name = $_REQUEST['name'];
+$tel  = $_REQUEST['tel'];
+
+$result = mysql_query("INSERT INTO address(no, name, tel) VALUES('$no', '$name', '$tel')", $con);
+if (!$result) {
+  exit('データを登録できませんでした。');
+}
+
+$con = mysql_close($con);
+if (!$con) {
+  exit('データベースとの接続を閉じられませんでした。');
+}
+
+?>
+<p>登録が完了しました。<br /><a href="index.html">戻る</a></p>
+</body>
+</html>
