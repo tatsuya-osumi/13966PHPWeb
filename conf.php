0b322a3928d3dff2f2aec545d984e4bbfba5fa3c
tatsuya-osumi <t.osumi@sankei.co.jp>
2017-03-15 14:09:30 +0900

    Update conf.php

--- conf.php
+++ conf.php
@@ -11,8 +11,8 @@
 <table border="1"><tr><td>
   <table>
    <tr><th>入館希望時間:</th><td><?php echo $_POST["day"] ?> <?php echo $_POST["time"] ?></td></tr>
-   <tr><th>ご芳名:</th><td><?php echo $_POST["name"] ?></td></tr>
-   <tr><th>ﾌﾘｶﾞﾅ:</th><td>ｾｲ:<?php echo $_POST["l_name"] ?>　ﾒｲ:<?php echo $_POST["f_name"] ?></td></tr>
+   <tr><th>ご芳名:</th><td><?php echo $_POST["l_name1"] ?> <?php echo $_POST["f_name1"] ?></td></tr>
+   <tr><th>ﾌﾘｶﾞﾅ:</th><td>ｾｲ:<?php echo $_POST["l_name2"] ?>　ﾒｲ:<?php echo $_POST["f_name2"] ?></td></tr>
    <tr><th>貴社名:</th><td><?php echo $_POST["company1"] ?></td></tr>
    <tr><th>ﾌﾘｶﾞﾅ:</th><td><?php echo $_POST["company2"] ?></td></tr>
    <tr><th>ﾒｰﾙｱﾄﾞﾚｽ:</th><td><?php echo $_POST["mail"] ?></td></tr>
