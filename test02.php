dadbfeb4a356d8e8d6ec770ef1eb3f2926dd4691
tatsuya-osumi <t.osumi@sankei.co.jp>
2017-04-25 00:12:42 +0900

    Update test02.php

--- test02.php
+++ test02.php
@@ -6,7 +6,7 @@
 <body>
 <?php
 
-$con = mysql_connect('us-cdbr-iron-east-03.cleardb.net', 'b1b8e502d991e6', 'e52e3992');
+$con = mysql_connect('us-cdbr-iron-east-03.cleardb.net:3306', 'b1b8e502d991e6', 'e52e3992');
 if (!$con) {
   exit('データベースに接続できませんでした。');
 }
